390765bb219ab8fdab87ec2725c54df5bdb6378d
rptl8sr <rptl8sr@gmail.com>
2024-07-08 20:28:49 +0300

    Update main.go with context and application initialization

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "go-effective-mobile/internal/logger"
+import (
+	"context"
+	"go-effective-mobile/internal/app"
+	"go-effective-mobile/internal/logger"
+	"os"
+)
 
 func main() {
 	logger.Init()
+
+	ctx, _ := context.WithCancel(context.Background())
+
+	a, err := app.New(ctx)
+	if err != nil {
+		logger.Error("Failed to initialize application", "error", err)
+		os.Exit(1)
+	}
+
 }
