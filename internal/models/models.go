62cab57c08c212feab1ed36fcf1e71ca5bd0a5f3
rptl8sr <rptl8sr@gmail.com>
2024-07-08 23:14:26 +0300

    Add models

---
+++ internal/models/models.go
@@ -0,0 +1,31 @@
+package models
+
+import "time"
+
+type User struct {
+	ID         int       `json:"id"`
+	Surname    string    `json:"surname"`
+	Name       string    `json:"name"`
+	Patronymic string    `json:"patronymic"`
+	Address    string    `json:"address"`
+	Passport   string    `json:"passport"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
+}
+
+type Status string
+
+var (
+	Created = Status("created")
+	Started = Status("started")
+	Stopped = Status("stopped")
+)
+
+type Task struct {
+	ID        int       `json:"id"`
+	UserID    int       `json:"user_id"`
+	Title     string    `json:"title"`
+	Status    Status    `json:"status"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
