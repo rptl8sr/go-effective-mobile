d32ae224255a7d54a2c9307b4ff2d01639898e42
rptl8sr <rptl8sr@gmail.com>
2024-07-08 20:03:06 +0300

    Add server main function and logger module

---
+++ internal/logger/logger.go
@@ -0,0 +1,34 @@
+package logger
+
+import (
+	"log/slog"
+	"os"
+	"sync"
+)
+
+var (
+	globalLogger *slog.Logger
+	once         sync.Once
+)
+
+func Init() {
+	once.Do(func() {
+		globalLogger = slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	})
+}
+
+func Debug(msg string, args ...any) {
+	globalLogger.Debug(msg, args...)
+}
+
+func Info(msg string, args ...any) {
+	globalLogger.Info(msg, args...)
+}
+
+func Warn(msg string, args ...any) {
+	globalLogger.Warn(msg, args...)
+}
+
+func Error(msg string, args ...any) {
+	globalLogger.Error(msg, args...)
+}
