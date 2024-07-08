b088fefe4114b74f3896b562b2fc21f4b1b379eb
rptl8sr <rptl8sr@gmail.com>
2024-07-08 20:09:16 +0300

    Add ping-pong handler

---
+++ internal/handlers/handlers.go
@@ -0,0 +1,8 @@
+package handlers
+
+import "net/http"
+
+func Pong(w http.ResponseWriter, _ *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write([]byte("ping"))
+}
--- internal/router/router.go
+++ internal/router/router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"github.com/go-chi/chi/v5"
+	"go-effective-mobile/internal/handlers"
 	"net/http"
 )
 
@@ -11,7 +12,7 @@ func New() *chi.Mux {
 	r.Get("/info", func(w http.ResponseWriter, r *http.Request) {})
 	r.Get("/users", func(w http.ResponseWriter, r *http.Request) {})
 	r.Get("/tasks", func(w http.ResponseWriter, r *http.Request) {})
-	r.Get("/ping", func(w http.ResponseWriter, r *http.Request) {})
+	r.Get("/ping", handlers.Pong)
 
 	return r
 }
